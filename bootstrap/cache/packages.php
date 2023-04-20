295b31a21d50b88c2311b57b6a40f08efd8909b5
Sidehustlefavorbankmotions <98602253+Sidehustlefavorbankmotions@users.noreply.github.com>
2023-04-21 00:59:14 +0200

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,168 @@
+<?php return array (
+  'anandsiddharth/laravel-paytm-wallet' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Anand\\LaravelPaytmWallet\\PaytmWalletServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'PaytmWallet' => 'Anand\\LaravelPaytmWallet\\Facades\\PaytmWallet',
+    ),
+  ),
+  'barryvdh/laravel-debugbar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Debugbar\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Debugbar' => 'Barryvdh\\Debugbar\\Facades\\Debugbar',
+    ),
+  ),
+  'facade/ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
+    ),
+  ),
+  'fruitcake/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'mews/purifier' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Mews\\Purifier\\PurifierServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Purifier' => 'Mews\\Purifier\\Facades\\Purifier',
+    ),
+  ),
+  'mollie/laravel-mollie' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Mollie\\Laravel\\MollieServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Mollie' => 'Mollie\\Laravel\\Facades\\Mollie',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nexmo/laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Nexmo\\Laravel\\NexmoServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Nexmo' => 'Nexmo\\Laravel\\Facade\\Nexmo',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'pnlinh/laravel-infobip-sms' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Pnlinh\\InfobipSms\\Providers\\InfobipSmsServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'InfobipSms' => 'Pnlinh\\InfobipSms\\Facades\\InfobipSms',
+    ),
+  ),
+  'pragmarx/google2fa-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'PragmaRX\\Google2FALaravel\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Google2FA' => 'PragmaRX\\Google2FALaravel\\Facade',
+    ),
+  ),
+  'spatie/laravel-cookie-consent' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\CookieConsent\\CookieConsentServiceProvider',
+    ),
+  ),
+  'spatie/laravel-permission' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Permission\\PermissionServiceProvider',
+    ),
+  ),
+  'victorybiz/laravel-crypto-payment-gateway' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Victorybiz\\LaravelCryptoPaymentGateway\\LaravelCryptoPaymentGatewayServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'LaravelCryptoPaymentGateway' => 'Victorybiz\\LaravelCryptoPaymentGateway\\LaravelCryptoPaymentGatewayFacade',
+    ),
+  ),
+);
